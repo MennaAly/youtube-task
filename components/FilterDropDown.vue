e5f47ce57f80444ef74591f8840f501cc63ad3a5
MennaAly <mennaali365@gmail.com>
2021-07-02 11:26:28 +0200

    ADDED filter drop down component

---
+++ components/FilterDropDown.vue
@@ -0,0 +1,28 @@
+<template>
+  <div data-app class="filter-drop-down">
+    <v-select
+      :items="filters"
+      :label="`${selectedFilter}`"
+      :menu-props="{ top: true, offsetY: true }"
+      background-color="#f8f8f8"
+      :height="10"
+      :width="10"
+      small-chips
+      dense
+      solo
+    >
+    </v-select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["filters"],
+  data() {
+    return {
+      closeOnContentClick: true,
+      selectedFilter: this.filters[0],
+    };
+  },
+};
+</script>

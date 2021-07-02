645b9a6d95efa95a08157194f01ffc07a5868cae
MennaAly <mennaali365@gmail.com>
2021-07-02 11:27:40 +0200

    ADDED default layout  component

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main-content">
+        <app-header/>
+        <div class="page-content">
+          <nuxt />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>

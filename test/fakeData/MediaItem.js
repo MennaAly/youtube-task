671d566e2a11679257a0e6e6e4ce297692f1cdd2
MennaAly <mennaali365@gmail.com>
2021-07-02 11:32:51 +0200

    ADDED media item fake data

---
+++ test/fakeData/MediaItem.js
@@ -0,0 +1,18 @@
+export default {
+    id: {
+        kind: 'youtube#video',
+        videoId: "video id 2"
+    },
+    snippet: {
+        title: 'title',
+        mediaDescribtion: 'media description',
+        channelTitle: 'channelTitle',
+        description: 'description',
+        publishedAt: '12 jun 2020',
+        thumbnails: {
+            medium: {
+                url: 'thumbnails medium url'
+            }
+        }
+    }
+}
